0dab229263cfa5316236d880aedfe3de4e41d80f
chaoschris <42533712+chaoschris@users.noreply.github.com>
2022-01-23 18:31:16 +0100

    Add files via upload

---
+++ software/V0.0.4b/firmware_V0.0.4c_speeduino-202008-IAT_as_Lambda_In/speeduino/board_stm32_official.h
@@ -0,0 +1,282 @@
+#ifndef STM32OFFICIAL_H
+#define STM32OFFICIAL_H
+#if defined(CORE_STM32_OFFICIAL)
+#include <Arduino.h>
+#include <HardwareTimer.h>
+#include <HardwareSerial.h>
+
+#if defined(STM32F1)
+#include "stm32f1xx_ll_tim.h"
+#elif defined(STM32F3)
+#include "stm32f3xx_ll_tim.h"
+#elif defined(STM32F4)
+#include "stm32f4xx_ll_tim.h"
+#else /*Default should be STM32F4*/
+#include "stm32f4xx_ll_tim.h"
+#endif
+/*
+***********************************************************************************************************
+* General
+*/
+#define PORT_TYPE uint32_t
+#define PINMASK_TYPE uint32_t
+#define COMPARE_TYPE uint16_t
+#define COUNTER_TYPE uint16_t
+#define micros_safe() micros() //timer5 method is not used on anything but AVR, the micros_safe() macro is simply an alias for the normal micros()
+#define TIMER_RESOLUTION 4
+
+
+#define USE_SERIAL3
+void initBoard();
+uint16_t freeRam();
+extern "C" char* sbrk(int incr);
+
+#if defined(ARDUINO_BLUEPILL_F103C8) || defined(ARDUINO_BLUEPILL_F103CB) \
+ || defined(ARDUINO_BLACKPILL_F401CC) || defined(ARDUINO_BLACKPILL_F411CE)
+  #ifndef PB11 //Hack for F4 BlackPills
+    #define PB11 PB10
+  #endif
+  //Hack to alow compile on small STM boards
+  #ifndef A10
+    #define A10  PA0
+    #define A11  PA1
+    #define A12  PA2
+    #define A13  PA3
+    #define A14  PA4
+    #define A15  PA5
+  #endif
+#endif
+
+#ifndef LED_BUILTIN
+  #define LED_BUILTIN PA7
+#endif
+
+/*
+***********************************************************************************************************
+* EEPROM emulation
+*/
+#if defined(SRAM_AS_EEPROM)
+    #define EEPROM_LIB_H "src/BackupSram/BackupSramAsEEPROM.h"
+    #include EEPROM_LIB_H
+    BackupSramAsEEPROM EEPROM;
+
+#elif defined(USE_SPI_EEPROM)
+    #define EEPROM_LIB_H "src/SPIAsEEPROM/SPIAsEEPROM.h"
+    #include EEPROM_LIB_H
+    SPIClass SPI_for_flash(PB5, PB4, PB3); //SPI1_MOSI, SPI1_MISO, SPI1_SCK
+ 
+    //windbond W25Q16 SPI flash EEPROM emulation
+    EEPROM_Emulation_Config EmulatedEEPROMMconfig{255UL, 4096UL, 31, 0x00100000UL};
+    Flash_SPI_Config SPIconfig{USE_SPI_EEPROM, SPI_for_flash};
+    SPI_EEPROM_Class EEPROM(EmulatedEEPROMMconfig, SPIconfig);
+
+#elif defined(FRAM_AS_EEPROM) //https://github.com/VitorBoss/FRAM
+    #define EEPROM_LIB_H <Fram.h>
+    #include EEPROM_LIB_H
+    #if defined(ARDUINO_BLACK_F407VE)
+      FramClass EEPROM(PB5, PB4, PB3, PB0); /*(mosi, miso, sclk, ssel, clockspeed) 31/01/2020*/
+    #else
+      FramClass EEPROM(PB15, PB14, PB13, PB12); //Blue/Black Pills
+    #endif
+
+#elif defined(STM32F7xx)
+  #define EEPROM_LIB_H "src/SPIAsEEPROM/SPIAsEEPROM.h"
+  #include EEPROM_LIB_H
+  #if defined(DUAL_BANK)
+    EEPROM_Emulation_Config EmulatedEEPROMMconfig{4UL, 131072UL, 2047UL, 0x08120000UL};
+  #else
+    EEPROM_Emulation_Config EmulatedEEPROMMconfig{2UL, 262144UL, 4095UL, 0x08180000UL};
+  #endif
+    InternalSTM32F7_EEPROM_Class EEPROM(EmulatedEEPROMMconfig);
+#else //default case, internal flash as EEPROM for STM32F4
+  #define EEPROM_LIB_H "src/SPIAsEEPROM/SPIAsEEPROM.h"
+  #include EEPROM_LIB_H
+    EEPROM_Emulation_Config EmulatedEEPROMMconfig{4UL, 131072UL, 2047UL, 0x08080000UL};
+    InternalSTM32F4_EEPROM_Class EEPROM(EmulatedEEPROMMconfig);
+#endif
+
+
+/*
+***********************************************************************************************************
+* Schedules
+*/
+#define MAX_TIMER_PERIOD 65535*4 //The longest period of time (in uS) that the timer can permit (IN this case it is 65535 * 4, as each timer tick is 4uS)
+#define uS_TO_TIMER_COMPARE(uS) (uS>>2) //Converts a given number of uS into the required number of timer ticks until that time has passed.
+
+#define FUEL1_COUNTER (TIM3)->CNT
+#define FUEL2_COUNTER (TIM3)->CNT
+#define FUEL3_COUNTER (TIM3)->CNT
+#define FUEL4_COUNTER (TIM3)->CNT
+
+#define FUEL1_COMPARE (TIM3)->CCR1
+#define FUEL2_COMPARE (TIM3)->CCR2
+#define FUEL3_COMPARE (TIM3)->CCR3
+#define FUEL4_COMPARE (TIM3)->CCR4
+
+#define IGN1_COUNTER  (TIM2)->CNT
+#define IGN2_COUNTER  (TIM2)->CNT
+#define IGN3_COUNTER  (TIM2)->CNT
+#define IGN4_COUNTER  (TIM2)->CNT
+
+#define IGN1_COMPARE (TIM2)->CCR1
+#define IGN2_COMPARE (TIM2)->CCR2
+#define IGN3_COMPARE (TIM2)->CCR3
+#define IGN4_COMPARE (TIM2)->CCR4
+
+#define FUEL5_COUNTER (TIM5)->CNT
+#define FUEL6_COUNTER (TIM5)->CNT
+#define FUEL7_COUNTER (TIM5)->CNT
+#define FUEL8_COUNTER (TIM5)->CNT
+
+#define FUEL5_COMPARE (TIM5)->CCR1
+#define FUEL6_COMPARE (TIM5)->CCR2
+#define FUEL7_COMPARE (TIM5)->CCR3
+#define FUEL8_COMPARE (TIM5)->CCR4
+
+#define IGN5_COUNTER  (TIM4)->CNT
+#define IGN6_COUNTER  (TIM4)->CNT
+#define IGN7_COUNTER  (TIM4)->CNT
+#define IGN8_COUNTER  (TIM4)->CNT
+
+#define IGN5_COMPARE (TIM4)->CCR1
+#define IGN6_COMPARE (TIM4)->CCR2
+#define IGN7_COMPARE (TIM4)->CCR3
+#define IGN8_COMPARE (TIM4)->CCR4
+
+  
+#define FUEL1_TIMER_ENABLE() (TIM3)->SR = ~TIM_FLAG_CC1; (TIM3)->DIER |= TIM_DIER_CC1IE
+#define FUEL2_TIMER_ENABLE() (TIM3)->SR = ~TIM_FLAG_CC2; (TIM3)->DIER |= TIM_DIER_CC2IE
+#define FUEL3_TIMER_ENABLE() (TIM3)->SR = ~TIM_FLAG_CC3; (TIM3)->DIER |= TIM_DIER_CC3IE
+#define FUEL4_TIMER_ENABLE() (TIM3)->SR = ~TIM_FLAG_CC4; (TIM3)->DIER |= TIM_DIER_CC4IE
+
+#define FUEL1_TIMER_DISABLE() (TIM3)->DIER &= ~TIM_DIER_CC1IE
+#define FUEL2_TIMER_DISABLE() (TIM3)->DIER &= ~TIM_DIER_CC2IE
+#define FUEL3_TIMER_DISABLE() (TIM3)->DIER &= ~TIM_DIER_CC3IE
+#define FUEL4_TIMER_DISABLE() (TIM3)->DIER &= ~TIM_DIER_CC4IE
+
+#define IGN1_TIMER_ENABLE() (TIM2)->SR = ~TIM_FLAG_CC1; (TIM2)->DIER |= TIM_DIER_CC1IE
+#define IGN2_TIMER_ENABLE() (TIM2)->SR = ~TIM_FLAG_CC2; (TIM2)->DIER |= TIM_DIER_CC2IE
+#define IGN3_TIMER_ENABLE() (TIM2)->SR = ~TIM_FLAG_CC3; (TIM2)->DIER |= TIM_DIER_CC3IE
+#define IGN4_TIMER_ENABLE() (TIM2)->SR = ~TIM_FLAG_CC4; (TIM2)->DIER |= TIM_DIER_CC4IE
+
+#define IGN1_TIMER_DISABLE() (TIM2)->DIER &= ~TIM_DIER_CC1IE
+#define IGN2_TIMER_DISABLE() (TIM2)->DIER &= ~TIM_DIER_CC2IE
+#define IGN3_TIMER_DISABLE() (TIM2)->DIER &= ~TIM_DIER_CC3IE
+#define IGN4_TIMER_DISABLE() (TIM2)->DIER &= ~TIM_DIER_CC4IE
+
+
+#define FUEL5_TIMER_ENABLE() (TIM5)->SR = ~TIM_FLAG_CC1; (TIM5)->DIER |= TIM_DIER_CC1IE
+#define FUEL6_TIMER_ENABLE() (TIM5)->SR = ~TIM_FLAG_CC2; (TIM5)->DIER |= TIM_DIER_CC2IE
+#define FUEL7_TIMER_ENABLE() (TIM5)->SR = ~TIM_FLAG_CC3; (TIM5)->DIER |= TIM_DIER_CC3IE
+#define FUEL8_TIMER_ENABLE() (TIM5)->SR = ~TIM_FLAG_CC4; (TIM5)->DIER |= TIM_DIER_CC4IE
+
+#define FUEL5_TIMER_DISABLE() (TIM5)->DIER &= ~TIM_DIER_CC1IE
+#define FUEL6_TIMER_DISABLE() (TIM5)->DIER &= ~TIM_DIER_CC2IE
+#define FUEL7_TIMER_DISABLE() (TIM5)->DIER &= ~TIM_DIER_CC3IE
+#define FUEL8_TIMER_DISABLE() (TIM5)->DIER &= ~TIM_DIER_CC4IE
+
+#define IGN5_TIMER_ENABLE() (TIM4)->SR = ~TIM_FLAG_CC1; (TIM4)->DIER |= TIM_DIER_CC1IE
+#define IGN6_TIMER_ENABLE() (TIM4)->SR = ~TIM_FLAG_CC2; (TIM4)->DIER |= TIM_DIER_CC2IE
+#define IGN7_TIMER_ENABLE() (TIM4)->SR = ~TIM_FLAG_CC3; (TIM4)->DIER |= TIM_DIER_CC3IE
+#define IGN8_TIMER_ENABLE() (TIM4)->SR = ~TIM_FLAG_CC4; (TIM4)->DIER |= TIM_DIER_CC4IE
+
+#define IGN5_TIMER_DISABLE() (TIM4)->DIER &= ~TIM_DIER_CC1IE
+#define IGN6_TIMER_DISABLE() (TIM4)->DIER &= ~TIM_DIER_CC2IE
+#define IGN7_TIMER_DISABLE() (TIM4)->DIER &= ~TIM_DIER_CC3IE
+#define IGN8_TIMER_DISABLE() (TIM4)->DIER &= ~TIM_DIER_CC4IE
+
+  
+
+
+/*
+***********************************************************************************************************
+* Auxilliaries
+*/
+#define ENABLE_BOOST_TIMER()  (TIM1)->SR = ~TIM_FLAG_CC2; (TIM1)->DIER |= TIM_DIER_CC2IE
+#define DISABLE_BOOST_TIMER() (TIM1)->DIER &= ~TIM_DIER_CC2IE
+
+#define ENABLE_VVT_TIMER()    (TIM1)->SR = ~TIM_FLAG_CC3; (TIM1)->DIER |= TIM_DIER_CC3IE
+#define DISABLE_VVT_TIMER()   (TIM1)->DIER &= ~TIM_DIER_CC3IE
+
+#define BOOST_TIMER_COMPARE   (TIM1)->CCR2
+#define BOOST_TIMER_COUNTER   (TIM1)->CNT
+#define VVT_TIMER_COMPARE     (TIM1)->CCR3
+#define VVT_TIMER_COUNTER     (TIM1)->CNT
+
+/*
+***********************************************************************************************************
+* Idle
+*/
+#define IDLE_COUNTER   (TIM1)->CNT
+#define IDLE_COMPARE   (TIM1)->CCR4
+
+#define IDLE_TIMER_ENABLE()  (TIM1)->SR = ~TIM_FLAG_CC4; (TIM1)->DIER |= TIM_DIER_CC4IE
+#define IDLE_TIMER_DISABLE() (TIM1)->DIER &= ~TIM_DIER_CC4IE
+
+/*
+***********************************************************************************************************
+* Timers
+*/
+
+HardwareTimer Timer1(TIM1);
+HardwareTimer Timer2(TIM2);
+HardwareTimer Timer3(TIM3);
+HardwareTimer Timer4(TIM4);
+#if !defined(ARDUINO_BLUEPILL_F103C8) && !defined(ARDUINO_BLUEPILL_F103CB) //F103 just have 4 timers
+HardwareTimer Timer5(TIM5);
+#if defined(TIM11)
+HardwareTimer Timer11(TIM11);
+#elif defined(TIM7)
+HardwareTimer Timer11(TIM7);
+#endif
+#endif
+
+#if ((STM32_CORE_VERSION_MINOR<=8) & (STM32_CORE_VERSION_MAJOR==1)) 
+void oneMSInterval(HardwareTimer*);
+void boostInterrupt(HardwareTimer*);
+void fuelSchedule1Interrupt(HardwareTimer*);
+void fuelSchedule2Interrupt(HardwareTimer*);
+void fuelSchedule3Interrupt(HardwareTimer*);
+void fuelSchedule4Interrupt(HardwareTimer*);
+#if (INJ_CHANNELS >= 5)
+void fuelSchedule5Interrupt(HardwareTimer*);
+#endif
+#if (INJ_CHANNELS >= 6)
+void fuelSchedule6Interrupt(HardwareTimer*);
+#endif
+#if (INJ_CHANNELS >= 7)
+void fuelSchedule7Interrupt(HardwareTimer*);
+#endif
+#if (INJ_CHANNELS >= 8)
+void fuelSchedule8Interrupt(HardwareTimer*);
+#endif
+void idleInterrupt(HardwareTimer*);
+void vvtInterrupt(HardwareTimer*);
+void ignitionSchedule1Interrupt(HardwareTimer*);
+void ignitionSchedule2Interrupt(HardwareTimer*);
+void ignitionSchedule3Interrupt(HardwareTimer*);
+void ignitionSchedule4Interrupt(HardwareTimer*);
+#if (IGN_CHANNELS >= 5)
+void ignitionSchedule5Interrupt(HardwareTimer*);
+#endif
+#if (IGN_CHANNELS >= 6)
+void ignitionSchedule6Interrupt(HardwareTimer*);
+#endif
+#if (IGN_CHANNELS >= 7)
+void ignitionSchedule7Interrupt(HardwareTimer*);
+#endif
+#if (IGN_CHANNELS >= 8)
+void ignitionSchedule8Interrupt(HardwareTimer*);
+#endif
+#endif //End core<=1.8
+
+/*
+***********************************************************************************************************
+* CAN / Second serial
+*/
+#if defined(ARDUINO_BLACK_F407VE)
+//HardwareSerial CANSerial(PD6, PD5);
+#endif
+
+#endif //CORE_STM32
+#endif //STM32_H
---
+++ software/V0.0.4b/firmware_V0.0.4c_speeduino-202008-IAT_as_Lambda_In/speeduino/scheduler.h
@@ -0,0 +1,264 @@
+/*
+This scheduler is designed to maintain 2 schedules for use by the fuel and ignition systems.
+It functions by waiting for the overflow vectors from each of the timers in use to overflow, which triggers an interrupt
+
+/Technical
+Currently I am prescaling the 16-bit timers to 256 for injection and 64 for ignition. This means that the counter increments every 16us (injection) / 4uS (ignition) and will overflow every 1048576uS
+Max Period = (Prescale)*(1/Frequency)*(2^17)
+(See playground.arduino.cc/code/timer1)
+This means that the precision of the scheduler is 16uS (+/- 8uS of target) for fuel and 4uS (+/- 2uS) for ignition
+
+/Features
+This differs from most other schedulers in that its calls are non-recurring (IE You schedule an event at a certain time and once it has occurred, it will not reoccur unless you explicitely ask for it)
+Each timer can have only 1 callback associated with it at any given time. If you call the setCallback function a 2nd time, the original schedule will be overwritten and not occur
+
+Timer identification
+The Arduino timer3 is used for schedule 1
+The Arduino timer4 is used for schedule 2
+Both of these are 16-bit timers (ie count to 65536)
+See page 136 of the processors datasheet: www.atmel.com/Images/doc2549.pdf
+
+256 prescale gives tick every 16uS
+256 prescale gives overflow every 1048576uS (This means maximum wait time is 1.0485 seconds)
+
+*/
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include "globals.h"
+
+#define USE_IGN_REFRESH
+#define IGNITION_REFRESH_THRESHOLD  30 //Time in uS that the refresh functions will check to ensure there is enough time before changing the end compare
+
+extern void (*inj1StartFunction)();
+extern void (*inj1EndFunction)();
+extern void (*inj2StartFunction)();
+extern void (*inj2EndFunction)();
+extern void (*inj3StartFunction)();
+extern void (*inj3EndFunction)();
+extern void (*inj4StartFunction)();
+extern void (*inj4EndFunction)();
+extern void (*inj5StartFunction)();
+extern void (*inj5EndFunction)();
+extern void (*inj6StartFunction)();
+extern void (*inj6EndFunction)();
+extern void (*inj7StartFunction)();
+extern void (*inj7EndFunction)();
+extern void (*inj8StartFunction)();
+extern void (*inj8EndFunction)();
+
+/** @name IgnitionCallbacks
+ * These are the function pointers that get called to begin and end the ignition coil charging. They are required for the various spark output modes
+*/
+///@{
+extern void (*ign1StartFunction)();
+extern void (*ign1EndFunction)();
+extern void (*ign2StartFunction)();
+extern void (*ign2EndFunction)();
+extern void (*ign3StartFunction)();
+extern void (*ign3EndFunction)();
+extern void (*ign4StartFunction)();
+extern void (*ign4EndFunction)();
+extern void (*ign5StartFunction)();
+extern void (*ign5EndFunction)();
+extern void (*ign6StartFunction)();
+extern void (*ign6EndFunction)();
+extern void (*ign7StartFunction)();
+extern void (*ign7EndFunction)();
+extern void (*ign8StartFunction)();
+extern void (*ign8EndFunction)();
+///@}
+
+void initialiseSchedulers();
+void beginInjectorPriming();
+void setFuelSchedule1(unsigned long timeout, unsigned long duration);
+void setFuelSchedule2(unsigned long timeout, unsigned long duration);
+void setFuelSchedule3(unsigned long timeout, unsigned long duration);
+void setFuelSchedule4(unsigned long timeout, unsigned long duration);
+//void setFuelSchedule5(void (*startCallback)(), unsigned long timeout, unsigned long duration, void(*endCallback)()); //Schedule 5 remains a special case for now due to the way it's implemented 
+void setFuelSchedule5(unsigned long timeout, unsigned long duration);
+void setFuelSchedule6(unsigned long timeout, unsigned long duration);
+void setFuelSchedule7(unsigned long timeout, unsigned long duration);
+void setFuelSchedule8(unsigned long timeout, unsigned long duration);
+void setIgnitionSchedule1(void (*startCallback)(), unsigned long timeout, unsigned long duration, void(*endCallback)());
+void setIgnitionSchedule2(void (*startCallback)(), unsigned long timeout, unsigned long duration, void(*endCallback)());
+void setIgnitionSchedule3(void (*startCallback)(), unsigned long timeout, unsigned long duration, void(*endCallback)());
+void setIgnitionSchedule4(void (*startCallback)(), unsigned long timeout, unsigned long duration, void(*endCallback)());
+void setIgnitionSchedule5(void (*startCallback)(), unsigned long timeout, unsigned long duration, void(*endCallback)());
+void setIgnitionSchedule6(void (*startCallback)(), unsigned long timeout, unsigned long duration, void(*endCallback)());
+void setIgnitionSchedule7(void (*startCallback)(), unsigned long timeout, unsigned long duration, void(*endCallback)());
+void setIgnitionSchedule8(void (*startCallback)(), unsigned long timeout, unsigned long duration, void(*endCallback)());
+
+inline void refreshIgnitionSchedule1(unsigned long timeToEnd) __attribute__((always_inline));
+
+//The ARM cores use seprate functions for their ISRs
+#if defined(CORE_STM32_OFFICIAL) || defined(CORE_STM32_GENERIC) || defined(CORE_TEENSY)
+  static inline void fuelSchedule1Interrupt();
+  static inline void fuelSchedule2Interrupt();
+  static inline void fuelSchedule3Interrupt();
+  static inline void fuelSchedule4Interrupt();
+#if (INJ_CHANNELS >= 5)
+  static inline void fuelSchedule5Interrupt();
+#endif
+#if (INJ_CHANNELS >= 6)
+  static inline void fuelSchedule6Interrupt();
+#endif
+#if (INJ_CHANNELS >= 7)
+  static inline void fuelSchedule7Interrupt();
+#endif
+#if (INJ_CHANNELS >= 8)
+  static inline void fuelSchedule8Interrupt();
+#endif
+#if (IGN_CHANNELS >= 1)
+  static inline void ignitionSchedule1Interrupt();
+#endif
+#if (IGN_CHANNELS >= 2)
+  static inline void ignitionSchedule2Interrupt();
+#endif
+#if (IGN_CHANNELS >= 3)
+  static inline void ignitionSchedule3Interrupt();
+#endif
+#if (IGN_CHANNELS >= 4)
+  static inline void ignitionSchedule4Interrupt();
+#endif
+#if (IGN_CHANNELS >= 5)
+  static inline void ignitionSchedule5Interrupt();
+#endif
+#if (IGN_CHANNELS >= 6)
+  static inline void ignitionSchedule6Interrupt();
+#endif
+#if (IGN_CHANNELS >= 7)
+  static inline void ignitionSchedule7Interrupt();
+#endif
+#if (IGN_CHANNELS >= 8)
+  static inline void ignitionSchedule8Interrupt();
+#endif
+#endif
+
+enum ScheduleStatus {OFF, PENDING, STAGED, RUNNING}; //The 3 statuses that a schedule can have
+
+struct Schedule {
+  volatile unsigned long duration;
+  volatile ScheduleStatus Status;
+  volatile byte schedulesSet; //A counter of how many times the schedule has been set
+  void (*StartCallback)(); //Start Callback function for schedule
+  void (*EndCallback)(); //Start Callback function for schedule
+  volatile unsigned long startTime; /**< The system time (in uS) that the schedule started, used by the overdwell protection in timers.ino */
+  volatile COMPARE_TYPE startCompare; //The counter value of the timer when this will start
+  volatile COMPARE_TYPE endCompare;
+
+  unsigned int nextStartCompare;
+  unsigned int nextEndCompare;
+  volatile bool hasNextSchedule = false;
+  volatile bool endScheduleSetByDecoder = false;
+};
+
+//Fuel schedules don't use the callback pointers, or the startTime/endScheduleSetByDecoder variables. They are removed in this struct to save RAM
+struct FuelSchedule {
+  volatile unsigned long duration;
+  volatile ScheduleStatus Status;
+  volatile byte schedulesSet; //A counter of how many times the schedule has been set
+  volatile COMPARE_TYPE startCompare; //The counter value of the timer when this will start
+  volatile COMPARE_TYPE endCompare;
+
+  unsigned int nextStartCompare;
+  unsigned int nextEndCompare;
+  volatile bool hasNextSchedule = false;
+};
+
+//volatile Schedule *timer3Aqueue[4];
+//Schedule *timer3Bqueue[4];
+//Schedule *timer3Cqueue[4];
+
+extern FuelSchedule fuelSchedule1;
+extern FuelSchedule fuelSchedule2;
+extern FuelSchedule fuelSchedule3;
+extern FuelSchedule fuelSchedule4;
+extern FuelSchedule fuelSchedule5;
+extern FuelSchedule fuelSchedule6;
+extern FuelSchedule fuelSchedule7;
+extern FuelSchedule fuelSchedule8;
+
+extern Schedule ignitionSchedule1;
+extern Schedule ignitionSchedule2;
+extern Schedule ignitionSchedule3;
+extern Schedule ignitionSchedule4;
+extern Schedule ignitionSchedule5;
+extern Schedule ignitionSchedule6;
+extern Schedule ignitionSchedule7;
+extern Schedule ignitionSchedule8;
+
+//IgnitionSchedule nullSchedule; //This is placed at the end of the queue. It's status will always be set to OFF and hence will never perform any action within an ISR
+
+static inline unsigned int setQueue(volatile Schedule *queue[], Schedule *schedule1, Schedule *schedule2, unsigned int CNT)
+{
+  //Create an array of all the upcoming targets, relative to the current count on the timer
+  unsigned int tmpQueue[4];
+
+  //Set the initial queue state. This order matches the tmpQueue order
+  if(schedule1->Status == OFF)
+  {
+    queue[0] = schedule2;
+    queue[1] = schedule2;
+    tmpQueue[0] = schedule2->startCompare - CNT;
+    tmpQueue[1] = schedule2->endCompare - CNT;
+  }
+  else
+  {
+    queue[0] = schedule1;
+    queue[1] = schedule1;
+    tmpQueue[0] = schedule1->startCompare - CNT;
+    tmpQueue[1] = schedule1->endCompare - CNT;
+  }
+
+  if(schedule2->Status == OFF)
+  {
+    queue[2] = schedule1;
+    queue[3] = schedule1;
+    tmpQueue[2] = schedule1->startCompare - CNT;
+    tmpQueue[3] = schedule1->endCompare - CNT;
+  }
+  else
+  {
+    queue[2] = schedule2;
+    queue[3] = schedule2;
+    tmpQueue[2] = schedule2->startCompare - CNT;
+    tmpQueue[3] = schedule2->endCompare - CNT;
+  }
+
+
+  //Sort the queues. Both queues are kept in sync.
+  //This implementes a sorting networking based on the Bose-Nelson sorting network
+  //See: pages.ripco.net/~jgamble/nw.html
+  #define SWAP(x,y) if(tmpQueue[y] < tmpQueue[x]) { unsigned int tmp = tmpQueue[x]; tmpQueue[x] = tmpQueue[y]; tmpQueue[y] = tmp; volatile Schedule *tmpS = queue[x]; queue[x] = queue[y]; queue[y] = tmpS; }
+  /*SWAP(0, 1); */ //Likely not needed
+  /*SWAP(2, 3); */ //Likely not needed
+  SWAP(0, 2);
+  SWAP(1, 3);
+  SWAP(1, 2);
+
+  //Return the next compare time in the queue
+  return tmpQueue[0] + CNT; //Return the
+}
+
+/*
+ * Moves all the Schedules in a queue forward one position.
+ * The current item (0) is discarded
+ * The final queue slot is set to nullSchedule to indicate that no action should be taken
+ */
+static inline unsigned int popQueue(volatile Schedule *queue[])
+{
+  queue[0] = queue[1];
+  queue[1] = queue[2];
+  queue[2] = queue[3];
+  //queue[3] = &nullSchedule;
+
+  unsigned int returnCompare;
+  if( queue[0]->Status == PENDING ) { returnCompare = queue[0]->startCompare; }
+  else { returnCompare = queue[0]->endCompare; }
+
+  return returnCompare;
+}
+
+
+#endif // SCHEDULER_H
